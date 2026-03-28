7cd68ed7cdda969861104304f8ce30060abff066
LuzApd <luzangelapd21@gmail.com>
2026-03-28 22:51:24 +0100

    addition default page

---
+++ resources/views/index.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Pagina Principal Agrivall</title>
+</head>
+<style>
+    *{margin: 0; padding: 0; box-sizing: border-box}
+    body{background: #a3bf50ff; color: #901b22ff; font-family: 'Montserrat' 'sans-serif'; font-size: 30px; text-align: center;; justify-content: center; margin-top: 200px;}
+    header {margin: 30px 0;}
+    footer{background: white; opacity: 0.5; position: fixed; bottom: 0; width: 100%; padding: 10px 0;}
+</style>
+<body>
+    <header>
+        <h1>AGRIVALL</h1>
+        <img src="../assets/cereza-default.png" alt="cerezas página en construcción" width="500" height="auto">
+    </header>
+    <main>
+        <h2>Aquí se construye la página de Agrivall</h2>
+    </main>
+    <footer>copyright &copy; 2026 Agrivall</footer>
+</body>
+</html>
